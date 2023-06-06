d54bf344f0b15be21425a92bdd3ba9c8ba8e86ac
Andrii-Pershko <andriiinf@gmail.com>
2023-06-06 19:03:08 +0300

    Update userContext.js

--- src/userContext.js
+++ src/userContext.js
@@ -5,10 +5,17 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const UserProvider = ({ children }) => {
-  const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(() => {
+    const theme = localStorage.getItem('darkTheme');
+    if (JSON.parse(theme)) {
+      return true;
+    }
+    return false;
+  });
 
   const toglleTheme = () => {
     setDarkTheme(!darkTheme);
+    localStorage.setItem('darkTheme', !darkTheme);
   };
 
   return (
